8a2c428c891b5a3647c58c977603474e75914e86
Georgii Zykov <gzykov@lt-m.ru>
2019-01-29 10:30:07 +0300

    Altered build path for Angular and altered repos for back

--- back/src/main/java/com/jeorgius/back/repos/ProductRepo.java
+++ back/src/main/java/com/jeorgius/back/repos/ProductRepo.java
@@ -1,11 +1,14 @@
 package com.jeorgius.back.repos;
 
 import com.jeorgius.back.entities.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 public interface ProductRepo extends CrudRepository<Product,Long> {
-
+    @Query("Select p from Product p order by product_id DESC")
+    List<Product> findProducts();
 }
