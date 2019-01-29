5ff87e3edd4c2d6007ed380b456923ce2f59a674
Georgii Zykov <gzykov@lt-m.ru>
2019-01-29 11:01:44 +0300

    Added Orders, Products and OrderDetail components

--- front-ts/src/app/app.module.ts
+++ front-ts/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
+import { ProductsComponent } from './products/products.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    OrdersComponent,
+    OrderDetailComponent
   ],
   imports: [
     BrowserModule,
