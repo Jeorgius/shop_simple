afea104149165a3bb9b6df6ab4affb64aaa8d710
Jeorgius <V0tum-Separatum@yandex.ru>
2019-01-30 18:07:39 +0300

    Created DbService and Rest GET resource

--- back/src/main/java/com/jeorgius/back/domain/DbService.java
+++ back/src/main/java/com/jeorgius/back/domain/DbService.java
@@ -1,7 +1,30 @@
 package com.jeorgius.back.domain;
 
+import com.jeorgius.back.domain.entities.Order;
+import com.jeorgius.back.domain.entities.Product;
+import com.jeorgius.back.domain.repos.OrderRepo;
+import com.jeorgius.back.domain.repos.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DbService {
+    private OrderRepo orderRepo;
+    private ProductRepo productRepo;
+
+    @Autowired
+    public DbService(OrderRepo orderRepo, ProductRepo productRepo) {
+        this.orderRepo = orderRepo;
+        this.productRepo = productRepo;
+    }
+
+    public List<Product> getProducts(){
+        return productRepo.findProducts();
+    }
+
+    public List<Order> getOrders(){
+        return orderRepo.findOrders();
+    }
 }
--- back/src/main/java/com/jeorgius/back/rest/RestGet.java
+++ back/src/main/java/com/jeorgius/back/rest/RestGet.java
@@ -1,13 +1,13 @@
 package com.jeorgius.back.rest;
 
 import com.jeorgius.back.domain.DbService;
+import com.jeorgius.back.domain.entities.Order;
 import com.jeorgius.back.domain.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 //Default values allows all requests for this class to be called
@@ -26,6 +26,11 @@ public class RestGet {
 
     @GetMapping("/products")
     public List<Product> showProducts(){
-        return new ArrayList<>();
+        return db.getProducts();
+    }
+
+    @GetMapping("/orders")
+    public List<Order> showOrders(){
+        return db.getOrders();
     }
 }
